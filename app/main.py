d3f462e63fa47b3bac3ff2091116e9e5145198bf
Andre Marcio <andre.marcio.ti@gmail.com>
2026-04-13 12:51:36 -0300

    verify task

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
-import app.players.player
-import app.players.elves.elf
-import app.players.dwarves.dwarf
+from app.players.player import Player
+from app.players.elves.elf import Elf
+from app.players.dwarves.dwarf import Dwarf
 
 
 def calculate_team_total_rating(team: list[Player]) -> int:
